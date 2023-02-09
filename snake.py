e98924689b451c92f74ef3adde8de9a4263d01a0
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-02-09 11:33:58 +0100

    Snake moves, random apple positions

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 window = pygame.display.set_mode((600, 500))
@@ -8,26 +9,41 @@ pygame.display.set_icon(icon)
 FPS = 30
 CLOCK = pygame.time.Clock()
 
-# Serpent commence à la taille de 1 bloc
+# Snake
 snake_size = 1
 snake_color = (70, 255, 0) # couleur serpent = vert
-
-apple_color = (255, 0, 0) # couleur pomme = rouge
+snake_x = 250
+snake_y = 200
+snake_speed = 5
 
 # Serpent avance toujours vers l'avant de 1 bloc
 
 # Le serpent doit manger des pommes qui apparaissent aléatoirement
 
-# Si le serpent mange une pomme: augmenter de 1 bloc sa taille, augmenter le score de 1
 # Si le serpent se touche lui même ou la bordure de la fenêtre, fin de la partie
+
+# Condition si serpent mange la pomme: augmente le score de 1, augmente le score et génère une nouvelle pomme à un x,y
+# random, augmenter de 1 bloc sa taille
+
+# Pomme
+apple_color = (255, 0, 0) # couleur pomme = rouge
+snake = pygame.draw.rect(window, apple_color, (random.randint(0, 600),random.randint(0, 500), 25, 25)) # x,y - width,height
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.draw.rect(window, snake_color, (250, 200, 25, 25)) # x,y - width,height
-    pygame.draw.rect(window, apple_color, (500, 500, 25, 25)) # x,y - width,height
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                snake_x += 30
+            if event.key == pygame.K_LEFT:
+                snake_x -= 30
+            if event.key == pygame.K_UP:
+                snake_y -= 30
+            if event.key == pygame.K_DOWN:
+                snake_y += 30
+    pygame.draw.rect(window, snake_color, (snake_x, snake_y, 25, 25)) # x,y - width,height
     pygame.display.flip()
     CLOCK.tick(FPS)
 pygame.quit()
