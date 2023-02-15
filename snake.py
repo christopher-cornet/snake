3c1511e9a492ad0abd1fd10b8ae9254129166c06
christopher-cornet <115154379+christopher-cornet@users.noreply.github.com>
2023-02-15 10:04:42 +0100

    Display the snake

--- snake.py
+++ snake.py
@@ -6,15 +6,20 @@ height = 480
 icon = pygame.image.load("snake.ico")
 pygame.display.set_icon(icon)
 
-grid_color = (216, 216, 216) # Couleur des lignes de la grille
+# Couleur des lignes de la grille
+grid_color = (216, 216, 216)
 
 # Taille des carrés dans la grille
 square_size = 32
+
 # Largeur et hauteur de la grille
 grid_width = width / square_size
 grid_height = height / square_size
 
 FPS = 10
+
+# Couleur du serpent
+snake_color = (22, 240, 26)
 
 # Jeu
 class Game():
@@ -31,8 +36,20 @@ class Game():
         while self.running:
             self.clock.tick(FPS)
             self.events()
+            self.update()
             self.display_grid()
 
+    # Mettre à jour le serpent
+    def update(self):
+        self.all_sprites.update()
+        self.head.x += 1
+
+    # Afficher le serpent
+    def snake_body(self):
+        self.all_sprites = pygame.sprite.Group()
+        self.head = Snake(self, 8, 7)
+
+    # Grille
     def grid(self):
         # Lignes de la grille
         for row in range(0, width, square_size):
@@ -40,18 +57,35 @@ class Game():
         for col in range(0, height, square_size):
             pygame.draw.line(self.window, grid_color, (0, col), (width, col)) # fenêtre, couleur, x,y 1er point, x,y 2e point
 
+    # Afficher la grille
     def display_grid(self):
-        # Afficher la grille
         self.grid()
+        self.all_sprites.draw(self.window)
         pygame.display.flip()
 
+    # Evenements
     def events(self):
-        # Evenements
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.quit()
 
+# Serpent
+class Snake(pygame.sprite.Sprite):
+    def __init__(self, game, x, y):
+        self.groups = game.all_sprites
+        pygame.sprite.Sprite.__init__(self, self.groups)
+        self.game = game
+        self.x, self.y = x, y # Coordonées du serpent
+        self.image = pygame.Surface((square_size, square_size))
+        self.image.fill(snake_color) # Couleur du serpent
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        self.rect.x = self.x * square_size
+        self.rect.y = self.y * square_size
+
 snake_game = Game()
 while True:
-    # Lancer le jeu
+    # Afficher le serpent sur le jeu, Lancer le jeu
+    snake_game.snake_body()
     snake_game.run()
